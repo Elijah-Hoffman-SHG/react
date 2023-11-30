a1d404c1355bf482bf1851c6012ae5debed8ee70
ehof16 <ehof@sandhills.com>
2023-11-29 17:00:45 -0700

    testing socket with snake game

---
+++ snake/src/sockettesting.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import './SocketTesting.css'
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:5174")
+
+
+function SocketTesting() {
+  const [room, setRoom] = useState("");
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  //can only emit message to a backend, so backend has to be middle man
+  const sendMessage = () =>{
+    socket.emit("send_message",  { message, room });
+  };
+  socket.on('updatePlayers', (players) =>{
+    console.log(players)
+  })
+  const joinRoom = () =>{
+    if(room !== ""){
+    socket.emit("join_room", room);
+    }
+  };
+
+  useEffect(()=>{
+    socket.on("receive_message", (data)=>{
+      setMessageReceived(data.message)
+    })
+
+  }, [socket])
+
+  return (
+    <>
+    <div className='SocketTesting'>
+    <input placeholder='Room number' onChange={(event)=>{
+        setRoom(event.target.value)
+      }}/>
+      <button onClick = {joinRoom}>Join Room</button>
+
+      <input placeholder='message' onChange={(event)=>{
+        setMessage(event.target.value)
+      }}/>
+      <button onClick = {sendMessage}>Send message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+    </>
+  )
+}
+
+export default SocketTesting
--- snake/src/titlescreen.jsx
+++ snake/src/titlescreen.jsx
@@ -1,11 +1,16 @@
 import "./Board.css"
 import BlockDescription from "./Blockdescription"
+import io from 'socket.io-client'
+import SocketTesting from "./sockettesting"
+const socket = io.connect("http://localhost:5174")
 
 function TitleScreen({ setGameStatus }) {
   
 
     return (
+        
         <div className="title-screen">
+            <SocketTesting/>
         <h1>Welcome to Elite Snake</h1>
        
         <BlockDescription/>
